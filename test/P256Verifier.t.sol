8ab38223dd020ab3e90fc22dbd3e886abb0c1fd1
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-03-18 18:29:15 +0100

    🔁 Upgrade Solidity Version to `0.8.25`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/P256Verifier.t.sol
+++ test/P256Verifier.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.25;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {stdJson} from "forge-std/StdJson.sol";
