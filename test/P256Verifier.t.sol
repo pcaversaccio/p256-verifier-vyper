9aa27839148ec0d55d6e51fdae21cbfda889dc6f
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-10-20 14:48:29 +0200

    🔁 Update Submodules
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/P256Verifier.t.sol
+++ test/P256Verifier.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.28;
 
 import {Test, console} from "forge-std/Test.sol";
 import {stdJson} from "forge-std/StdJson.sol";
